36021fbc05259c4a17ce519b2787191f1356c099
Ashik Uddin Ahmad <ashikcu@gmail.com>
2014-12-19 20:40:39 +0600

    First commit. Initially working with original prayer code

--- SalahTime/ViewController.m
+++ SalahTime/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "PrayTime.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    PrayTime *pt = [PrayTime new];
+    /*
+     calcMethod 	= 1
+     asrMethod  	= 1
+     latitude   	= 23.70
+     longitude  	= 90.37
+     timezone 	= +6
+     */
+    [pt setCalcMethod:1];
+    [pt setAsrMethod:1];
+    NSArray *times = [pt getDatePrayerTimes:2014
+                                   andMonth:12
+                                     andDay:19
+                                andLatitude:23.70
+                               andLongitude:90.37
+                                andtimeZone:6];
+    NSLog(@"%@", times);
 }
 
 - (void)didReceiveMemoryWarning {
