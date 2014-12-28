f34363d3be4b04107d7e8f571bd5cdedb9444a17
Ashik Uddin Ahmad <ashikcu@gmail.com>
2014-12-28 13:48:19 +0600

    Added settings screen

--- SalahTime/ViewController.m
+++ SalahTime/ViewController.m
@@ -11,6 +11,16 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *fajrDetails;
+@property (weak, nonatomic) IBOutlet UILabel *sunriseDetails;
+@property (weak, nonatomic) IBOutlet UILabel *dhuhrDetails;
+@property (weak, nonatomic) IBOutlet UILabel *asrDetails;
+@property (weak, nonatomic) IBOutlet UILabel *sunsetDetails;
+@property (weak, nonatomic) IBOutlet UILabel *maghribDetails;
+@property (weak, nonatomic) IBOutlet UILabel *ishaDetails;
+
+@property (strong, nonatomic) PrayTime *prayTime;
+
 @end
 
 @implementation ViewController
@@ -18,7 +28,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    PrayTime *pt = [PrayTime new];
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
     /*
      calcMethod 	= 1
      asrMethod  	= 1
@@ -26,20 +39,46 @@
      longitude  	= 90.37
      timezone 	= +6
      */
-    pt.calcMethod = PTKCalculationMethodKarachi;
-    pt.asrJuristic = PTKJuristicMethodHanafi;
-    NSArray *times = [pt getDatePrayerTimes:2014
-                                   andMonth:12
-                                     andDay:19
-                                andLatitude:23.70
-                               andLongitude:90.37
-                                andtimeZone:6];
-    NSLog(@"%@", times);
+    self.prayTime = [PrayTime new];
+    self.prayTime.calcMethod = PTKCalculationMethodKarachi;
+    self.prayTime.asrJuristic = PTKJuristicMethodHanafi;
+    self.prayTime.timeFormat = PTKTimeFormatTime12WithSuffix;
+    [self refreshTimes:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - User Acions
+
+- (IBAction)refreshTimes:(id)sender {
+    NSCalendar *cal = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *components = [cal components:(NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay) fromDate:[NSDate date]];
+    NSArray *times = [self.prayTime getPrayerTimes:components
+                                       andLatitude:23.70
+                                      andLongitude:90.37
+                                       andtimeZone:6];
+    
+    NSArray *labels = @[self.fajrDetails, self.sunriseDetails, self.dhuhrDetails, self.asrDetails, self.sunsetDetails, self.maghribDetails, self.ishaDetails];
+    for (int i=0; i<labels.count; i++) {
+        ((UILabel *)labels[i]).text = times[i];
+    }
+    NSLog(@"%@", times);
+}
+
+#pragma mark - TableView Delegate/DataSource
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSArray *hideArr;
+    if(!hideArr) hideArr = @[@1, @4];
+    
+    if ([hideArr containsObject:@(indexPath.row)]) {
+        return 0;
+    } else {
+        return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+    }
 }
 
 @end
